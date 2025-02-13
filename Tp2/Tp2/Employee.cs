de11cfeb8cd5ab2fef4ec937f867088bf49bc104
OUMIF <eladnanimehdi03@gmail.com>
2025-02-13 15:16:33 +0100

    j'ai initialiser le projet

--- Tp2/Tp2/Employee.cs
+++ Tp2/Tp2/Employee.cs
@@ -1,20 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Employee
+namespace test
 {
-	public Employee()
-	{
+    internal class Employee
+    {
 
-		private string nom;
-	    private float salaire;
-     	private string poste;
-	    private string date;
+        private string nom;
+        private float salaire;
+        private string poste;
+        private string date;
+       
+
+        
+        
+        
+        public Employee(string nom, float salaire, string poste, string date)
+        {
+            this.nom = nom;
+            this.Salaire = salaire;
+            this.Poste = poste;
+            this.date = date;
+           
+        
+        }
 
 
 
 
-	     
 
 
-	}
+        public string Date { get => date; set => date = value; }
+        public string Poste { get => poste; set => poste = value; }
+        public float Salaire { get => salaire; set => salaire = value; }
+        public string Nom { get => nom; set => nom = value; }
+    }
 }
--- Tp2/Tp2/GestionEmployes.cs
+++ Tp2/Tp2/GestionEmployes.cs
@@ -1,10 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
 
-public class GestionEmployes
+namespace test
 {
-	public GestionEmployes()
-	{
+    internal class GestionEmployes
+    {
+
+        private List<Employee> employees;
+        private static int nombreEmployee;
+        static float sommeSalaire;
 
 
-	}
+        public GestionEmployes(List<Employee> employees)
+        {
+            this.employees = employees;
+            nombreEmployee = 0;
+            sommeSalaire = 0;
+        }
+
+        public bool Ajouter(Employee employee)
+        {
+            foreach (Employee emp in employees)
+            {
+                if (emp.Nom.Equals(employee.Nom))
+                {
+                    return false;
+                }
+            }
+            employees.Add(employee);
+            sommeSalaire += employee.Salaire;
+            nombreEmployee++;
+            return true;
+
+        }
+
+        
+        public bool supprimer(Employee employee)
+        {
+
+            foreach (Employee emp in employees)
+            {
+                if (emp.Nom.Equals(employee.Nom))
+                {
+                    nombreEmployee--;
+                    sommeSalaire -= emp.Salaire;    
+                    employees.Remove(emp);
+                    return true;
+                }  
+            }
+            return false;
+        }
+
+        
+        static public float calculerSalaireMoyen()
+        {
+            return sommeSalaire/nombreEmployee;
+        }
+
+
+
+        public static int NombreEmployee { get => nombreEmployee; set => nombreEmployee = value; }
+        internal List<Employee> Employees { get => employees; set => employees = value; }
+        public static float SommeSalaire { get => sommeSalaire; set => sommeSalaire = value; }
+    }
 }
