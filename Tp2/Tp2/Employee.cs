e8e4b347f2bf0149fa7374cfc7372b34551ac36d
OUMIF <eladnanimehdi03@gmail.com>
2025-02-13 15:11:08 +0100

    Initial commit

---
+++ Tp2/Tp2/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Employee
+{
+	public Employee()
+	{
+
+		private string nom;
+	    private float salaire;
+     	private string poste;
+	    private string date;
+
+
+
+
+	     
+
+
+	}
+}
